c7b342125df0e19e3b9e02bbf449ff082163acdf
epasabuyph <66150422+epasabuyph@users.noreply.github.com>
2023-10-16 10:14:36 +0800

    Update Layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,47 +1,23 @@
-import React from 'react';
-import Helmet from 'react-helmet';
-import PropTypes from 'prop-types';
+import React from "react"
 
-import Header from '../Header';
-import Footer from '../Footer';
-import * as styles from './Layout.module.css';
+const Layout = ({ children }) => (
+  <div>
+    <header>
+      <h1>E-Hailing Service</h1>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About Us</a></li>
+          <li><a href="/services">Our Services</a></li>
+          <li><a href="/contact">Contact Us</a></li>
+        </ul>
+      </nav>
+    </header>
+    <div id="content">{children}</div>
+    <footer>
+      <p>&copy; {new Date().getFullYear} E-Hailing Service</p>
+    </footer>
+  </div>
+)
 
-// CSS not modular here to provide global styles
-import './Globals.css';
-
-const Layout = ({ props, children, disablePaddingBottom = false }) => {
-  return (
-    <>
-      <Helmet>
-        {/* Add any sitewide scripts here */}
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-        />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          charset="UTF-8"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-        />
-      </Helmet>
-
-      <Header />
-      <main
-        className={`${styles.main} ${
-          disablePaddingBottom === true ? styles.disablePaddingBottom : ''
-        }`}
-      >
-        {children}
-      </main>
-      <Footer />
-    </>
-  );
-};
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-};
-
-export default Layout;
+export default Layout
